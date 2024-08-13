ba8a5aa59ba589e3c91d77dbbff1405735fb8b1a
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-08-13 14:39:33 +0000

    modernize resource info procs

--- tcl/init-procs.tcl
+++ tcl/init-procs.tcl
@@ -1,7 +1,15 @@
 namespace eval openacs_bootstrap3_theme {
-    set bootstrap_version 3.4.1
-
-    ad_proc -private ::openacs_bootstrap3_theme::resource_info {} {
+    variable parameter_info
+    
+    set parameter_info {
+        package_key openacs-bootstrap3-theme
+        parameter_name BootstrapVersion
+        default_value 3.4.1
+    }
+    
+    ad_proc -private ::openacs_bootstrap3_theme::resource_info {
+        {-version ""}
+    } {
 
 	Get information about available version(s) of the bootstrap
 	packages. The goal is to deliver resources either from the
@@ -10,13 +18,22 @@ namespace eval openacs_bootstrap3_theme {
 	@return dict containing resourceDir, resourceName, cdn,
 		cdnHost, prefix, cssFiles, jsFiles and extraFiles.
 
-    } {
+    } {      
+        variable parameter_info
+        if {$version eq ""} {
+            dict with parameter_info {
+                set version [::parameter::get_global_value \
+                                 -package_key $package_key \
+                                 -parameter $parameter_name \
+                                 -default $default_value]
+            }
+        }
+        
 	#
 	# Provide paths for loading either via resources or CDN
 	#
 	set resourceDir [acs_package_root_dir openacs-bootstrap3-theme/www/resources/bootstrap]
 	set cdn         //maxcdn.bootstrapcdn.com/bootstrap
-	set version     $openacs_bootstrap3_theme::bootstrap_version
 
 	if {[file exists $resourceDir/$version]} {
 	    set prefix  /resources/openacs-bootstrap3-theme/bootstrap/$version
--- tcl/resource-init.tcl
+++ tcl/resource-init.tcl
@@ -1,24 +1,7 @@
 #
-# Register some URNs that we are providing for sharing.
+# Register some URNs
 #
-
-set version $::openacs_bootstrap3_theme::bootstrap_version
-
-#
-# Get resource info from this package. If not locally installed, offer
-# a link for download.
-#
-set resource_info [::openacs_bootstrap3_theme::resource_info]
-set prefix        [dict get $resource_info prefix]
-
-foreach URN [dict keys [dict get $resource_info urnMap]] {
-    template::register_urn \
-        -urn $URN \
-        -resource $prefix/[dict get $resource_info urnMap $URN] \
-        -csp_list [expr {[dict exists $resource_info cspMap $URN]
-                         ? [dict get $resource_info cspMap $URN]
-                         : ""}]
-}
+::util::resources::register_urns -namespace openacs_bootstrap3_theme
 
 template::register_urn \
     -urn urn:ad:js:jquery \
