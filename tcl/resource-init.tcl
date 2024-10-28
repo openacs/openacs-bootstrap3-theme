95b63058c289e5e1c03d754b81bdcb9c51e1e298
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-10-28 17:09:41 +0000

    added theme specific registry for cass class name mapping

--- tcl/resource-init.tcl
+++ tcl/resource-init.tcl
@@ -7,6 +7,19 @@ template::register_urn \
     -urn urn:ad:js:jquery \
     -resource /resources/openacs-bootstrap3-theme/js/jquery-3.7.1.min.js
 
+::template::CSS registerCSSclasses bootstrap {
+    action "btn btn-default"
+    btn-default btn-default
+    bulk-action "btn btn-default"
+    card "panel panel-default"
+    card-body panel-body
+    card-header panel-heading
+    d-none hidden
+    form-action "btn btn-default"
+    margin-form ""
+    text-warning text-warn
+}
+
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
